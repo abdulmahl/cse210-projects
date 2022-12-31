7f2c93737c574b94a5826eccfaab2a977e081431
Duke4Coco <u531156798@byui.edu>
2022-12-31 11:51:25 +0200

    Separated the lines of my code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,8 +6,10 @@ class Program
     {
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
+
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
+        
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
