5f84f03a5db8620d2abb874cede28d47e9116d80
Duke4Coco <u531156798@byui.edu>
2023-01-17 10:28:25 +0200

    Sandbox prctices.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,45 +5,45 @@ class Program
     static void Main(string[] args)
     {
 
-        List <string> colors;
-        colors = new List <string> ();
+        // List <string> colors;
+        // colors = new List <string> ();
 
-        for (int i = 0; i < 12; i = i + 3)
-        {
-            Console.WriteLine(i);
-        }
+        // for (int i = 0; i < 12; i = i + 3)
+        // {
+        //     Console.WriteLine(i);
+        // }
 
-        string input = "yes";
+        // string input = "yes";
   
-        while (input == "yes")
-        {
-            Console.Write("Do you want to continue? ");
-            input = Console.ReadLine();
-        }
+        // while (input == "yes")
+        // {
+        //     Console.Write("Do you want to continue? ");
+        //     input = Console.ReadLine();
+        // }
 
-        colors.Add("pink");
-        colors.Add("green");
-        colors.Add("black");
-        colors.Add("blue");
+        // colors.Add("pink");
+        // colors.Add("green");
+        // colors.Add("black");
+        // colors.Add("blue");
 
-        foreach (string color in colors)
-        {
-            Console.WriteLine(color);
-        }
+        // foreach (string color in colors)
+        // {
+        //     Console.WriteLine(color);
+        // }
 
-        Console.WriteLine(colors.Count);
+        // Console.WriteLine(colors.Count);
 
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,12);
-        {
-            Console.WriteLine(number);
-        }
+        // Random randomGenerator = new Random();
+        // int number = randomGenerator.Next(1,12);
+        // {
+        //     Console.WriteLine(number);
+        // }
 
-        for (int i = 0; i < colors.Count; i++)
-        {
+        // for (int i = 0; i < colors.Count; i++)
+        // {
 
-        }
-        Console.WriteLine(colors[3]);
- 
+        // }
+        // Console.WriteLine(colors[3]);
+
     }
 }
