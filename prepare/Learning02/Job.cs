0b6c9c98a866e5d2594d5e672c936fb78eaa5eb1
Duke4Coco <u531156798@byui.edu>
2023-01-20 17:36:57 +0200

    Modified a few lines of code.

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,15 +2,14 @@ using System;
 
 public class Job
     {
-        public string _companyName;
-        public string _jobTitle;
+        public string _companyName = "";
+        public string _jobTitle = "";
         public int _startYear;
         public int _endYear;
 
         public void Display()
         {
-            Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endYear}");
+            Console.WriteLine($"{_companyName} {_jobTitle}, {_startYear}-{_endYear}");
         }
-
 
     }
