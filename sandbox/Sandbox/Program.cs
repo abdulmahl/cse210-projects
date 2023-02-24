4af147ad7ca9f634168837aba9ec9ee139f020b9
Duke4Coco <u531156798@byui.edu>
2023-02-24 22:14:18 +0200

    Emptied my Sandbox file.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -8,14 +8,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        string text = "Lorem ipsum dolor...";
-
-        for (int i = 1; i <= text.Length; i++)
-        {
-            Console.Write(text.Substring(0, i));
-            Thread.Sleep((int)(1000 * Math.Pow(1.1, -i)));
-        }
-
-        Console.ReadLine();
+        
     }
 }
